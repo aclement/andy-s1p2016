935db4c9279aecbfc3ce4ee47eccccf716efc973
Andy Clement <aclement@pivotal.io>
2016-08-03 09:00:34 -0700

    polish

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,6 +3,15 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Eclipse Neon: new and noteworthy:
+ *   https://www.eclipse.org/eclipse/news/4.6/
+ * 
+ * STS 3.8.1 New and Noteworthy:
+ *   http://docs.spring.io/sts/nan/latest/NewAndNoteworthy.html
+ * 
+ * @author Andy Clement
+ */
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,6 +19,6 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	private void m() {
+	public void play() {
 	}
 }
